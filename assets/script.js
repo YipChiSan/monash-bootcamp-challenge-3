0e58e1e6253ec5348fa448821a01b874fcf7e2a9
YipChiSan <zye39@yahoo.com>
2022-06-06 21:43:00 +1000

    Rewrite the hardcode part

--- assets/script.js
+++ assets/script.js
@@ -3,9 +3,11 @@ function generatePassword() {
   let countRequiredConditions;
   let errorMessage;
   let haveLowerCase, haveUpperCase, haveNumbers, haveSpecial;
-  let englishLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  let specialChars = [" ", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", "\,", "-", "\.", "\/", "\:", "\;", "<", "\=", ">", "?", "@", "[", "\\", "]", "^", "_", "\`", "{", "\|", "\}", "~"];
+  let charBase = [["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+                  ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+                  ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+                  [" ", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", "\,", "-", "\.", "\/", "\:", "\;", "<", "\=", ">", "?", "@", "[", "\\", "]", "^", "_", "\`", "{", "\|", "\}", "~"]
+                  ];
   let hasPasswordLengthError = false;
   let hasConditionsError = false;
   let passwordLengthError = "Please include a positive integer in your input.";
