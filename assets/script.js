ba0175b8c1ae338dca7182d7bf4790f8acfad1d2
YipChiSan <zye39@yahoo.com>
2022-06-06 20:45:01 +1000

    Hardcode all characters needed to generate password

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,9 @@ function generatePassword() {
   let countRequiredConditions;
   let errorMessage;
   let haveLowerCase, haveUpperCase, haveNumbers, haveSpecial;
+  let englishLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  let specialChars = [" ", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", "\,", "-", "\.", "\/", "\:", "\;", "<", "\=", ">", "?", "@", "[", "\\", "]", "^", "_", "\`", "{", "\|", "\}", "~"];
   let hasPasswordLengthError = false;
   let hasConditionsError = false;
   let passwordLengthError = "Please include a positive integer in your input.";
